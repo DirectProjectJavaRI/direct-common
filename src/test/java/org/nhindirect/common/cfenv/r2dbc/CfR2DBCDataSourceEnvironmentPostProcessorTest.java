56ce65fc791d2991dfc2b9444c25f04d8956e294
gm2552 <gm2552@cern-demo16.northamerica.cerner.net>
2020-01-23 11:45:08 -0600

    Major version upgrade to 7.0
    - Updating to SpringBoot 2.2.x
    - Adding java cfenv processor for jdbc to r2dbc.

---
+++ src/test/java/org/nhindirect/common/cfenv/r2dbc/CfR2DBCDataSourceEnvironmentPostProcessorTest.java
@@ -0,0 +1,44 @@
+package org.nhindirect.common.cfenv.r2dbc;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.nio.file.Files;
+
+import org.junit.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.util.ResourceUtils;
+
+import io.pivotal.cfenv.test.AbstractCfEnvTests;
+
+public class CfR2DBCDataSourceEnvironmentPostProcessorTest extends AbstractCfEnvTests
+{
+	private final CfR2DBCDataSourceEnvironmentPostProcessor environmentPostProcessor = new CfR2DBCDataSourceEnvironmentPostProcessor();
+
+	private final ConfigurableApplicationContext context = new AnnotationConfigApplicationContext();
+	
+	@Test
+	public void testParseVCAPToR2dbcProps() throws Exception
+	{
+		System.setProperty("VCAP_APPLICATION", "yes");
+		
+		try
+		{
+			// To setup values used by CfEnv
+			File file = ResourceUtils.getFile("classpath:cf/vcap-services.json");
+			String fileContents = new String(Files.readAllBytes(file.toPath()));
+			mockVcapServices(fileContents);
+			
+			environmentPostProcessor.postProcessEnvironment(this.context.getEnvironment(), null);
+			
+			final String url = this.context.getEnvironment().getProperty("spring.r2dbc.url");
+
+			assertEquals("r2dbcs:mysql://10.0.4.35:3306/cf_2e23d10a_8738_8c3c_66cf_13e44422698c?useSSL=true&requireSSL=true", url);
+		}
+		finally
+		{
+			System.clearProperty("VCAP_APPLICATION");
+		}
+	}
+}
