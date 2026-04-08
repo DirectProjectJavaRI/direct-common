7bfdd064271c52aea7b203552e0bd2158910002b
Greg Meyer <abareasol@gmail.com>
2026-04-08 05:36:13 -0500

    Adding timeouts in the web client to defned against infinite hangs.

--- src/main/java/org/nhindirect/common/rest/exchange/DirectWebClientBuilderConfig.java
+++ src/main/java/org/nhindirect/common/rest/exchange/DirectWebClientBuilderConfig.java
@@ -1,17 +1,22 @@
 package org.nhindirect.common.rest.exchange;
 
+import java.time.Duration;
+
 import org.nhindirect.common.rest.exceptions.AuthorizationException;
 import org.nhindirect.common.rest.exceptions.ServiceMethodException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.util.StringUtils;
 import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunctions;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import io.netty.channel.ChannelOption;
 import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
 
 @Configuration
 public class DirectWebClientBuilderConfig {
@@ -26,6 +31,12 @@ public class DirectWebClientBuilderConfig {
 	@Value("${direct.webservices.retry.backoff.maxInterval:20000}")
 	protected long maxInterval;		
 	
+	@Value("${direct.webservices.connect.timeout:5000}")                                                                                                                                                                      
+	protected int connectTimeoutMs;
+
+	@Value("${direct.webservices.response.timeout:10000}")                                                                                                                                                                    
+	protected long responseTimeoutMs;
+	
 	@Value("${direct.webservices.security.basic.user.name:}")
 	protected String user;	
 	
@@ -37,7 +48,13 @@ public class DirectWebClientBuilderConfig {
 		
 		// TODO: Add retry configuration
 		
-		WebClient.Builder builder = WebClient.builder();
+        HttpClient httpClient = HttpClient.create()
+              .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, connectTimeoutMs)
+              .responseTimeout(Duration.ofMillis(responseTimeoutMs));                                                                                                                                                           
+       
+        WebClient.Builder builder = WebClient.builder()                                                                                                                                                                       
+              .clientConnector(new ReactorClientHttpConnector(httpClient));
+
 		
 		if (StringUtils.hasText(user))
 			builder.filter(ExchangeFilterFunctions.basicAuthentication(user, pass));
